20ec5ec7377456b2a665d6b8cfbed82ea881bef4
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2020-04-30 22:38:40 -0300

    removing capital letters and exchanging space for hyphens

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 const fs = require('fs');
 
+// path to be searched
+const path = "/home/jonnathan/mega/estudos/my-projects";
 
-const path = "./";
-
+// logic for changing names
 fs.readdir(path,(error, values)=>{
     listCurrentNames(path);
-    values.map(value => {
-        const newValue = value.toLowerCase();
-        rename(value, newValue); 
-    });
+    values.map(value => setNew(value));
     listCurrentNames(path);
 })
-
-// function that renames the names
-function rename(oldValue, newValue){
-    fs.renameSync(oldValue, newValue, () => log(`${value} changed to ${newValue}`)); 
+// configure and change to the new name
+function setNew(name){
+    const newName = name.toLowerCase().trim().replace(' ', '-');
+    rename(path, name, newName); 
 }
-     
-// function to list current names in informed path 
+// change folder and file names
+function rename(path, oldValue, newValue){
+    path = path[path.length-1] === '/' ? path : path + '/';
+    const oldPath = `${path}/${oldValue}`;
+    const newPath = `${path}/${newValue}`;
+    fs.renameSync(oldPath, newPath, () => log(`${value} changed to ${newValue}`)); 
+}
+// lists the files and folders in the given directory
 function listCurrentNames(path) { 
     log("\nCurrent Names:\n"); 
     fs.readdirSync(path).forEach(value => log(value)); 
 } 
-function log(data){ 
-    console.log(data) 
-};
+// alias console.log
+function log(data){  console.log(data) };
