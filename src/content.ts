0a5d2491c2f1d4905550414291f0da2b9e7498cc
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2020-06-18 17:37:04 -0300

    code spliting and refactoring of the content class

--- src/content.ts
+++ src/content.ts
@@ -1,27 +1,30 @@
-const ALPHABET = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+$/;
+const ALPHABET = /^[A-zÀ-ỹ]+$/;
+
+const removeAccents = (str: string): string => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+const isCapitalLetter = (str: string): boolean => isLetterOfAlphabet(str) ? str === str.toUpperCase() : false;
+const isEmpty = (value: string): boolean => !value.trim();
+const isLetterOfAlphabet = (value: string): boolean => !!value.match(ALPHABET);
+
+const transformToLowerCaseWithHyphen = (str: string): string => {
+	let newValue: string = str[0];
+	if (str.length === 1) return str;
+	if (str.length === 1) return str;
+	for (let i = 1; i < str.length; i++) {
+		let transform = isCapitalLetter(str[i]) && isLetterOfAlphabet(str[i]) && isLetterOfAlphabet(str[i-1]);
+		newValue += transform ? `-${str[i]}` : str[i];		
+	}		
+	return newValue.toLowerCase();
+}
 
 export class Content{
-
-    constructor (private value: string){ 
-        const empty = !value.trim();
-        if (empty) throw new EvalError("Invalid Content");
-    }
-    old = () => this.value;
-    new = () => {
-		let previous: string = "";
-		const match = (str: string) => str.match(ALPHABET);
-		const isUpperCase = (e: string) => e === e.toUpperCase();
-		const transform = (x: string) => match(x) && isUpperCase(x) && match(previous);
-		let newContent = this.value.split("").map(value => {
-			const newValue = transform(value) ? `-${value}` : value;
-			previous = value;
-			return newValue;
-		}).join("");
-		return newContent
-			.toLowerCase()
-			.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // Remove accents
+	private newValue: string;
+    constructor (public value: string){     
+		if (isEmpty(value)) throw new EvalError("Content cannot be EMPTY"); 
+		this.newValue = removeAccents(value)
 			.replace(/\-\-+/g, '-')	// Replaces multiple hyphens with a single hyphen
 			.replace(/([^\w]+|\s+)/g, '-') // Replaces space and other characters with a hyphen
 			//.replace(/(^-+|-+$)/, ''); // Remove extra hyphens from the end or beginning of the string
 	}
+    old = () => this.value;
+	new = () => transformToLowerCaseWithHyphen(this.newValue);			
 }
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
 import { Path } from '../src/path'
-import  { mkdirSync, readdirSync, existsSync} from 'fs';
+import  { mkdirSync, readdirSync, existsSync, rmdirSync} from 'fs';
 
 const ROOT = "./test/example/";
 
@@ -9,6 +9,21 @@ abstract class Root {
         if (!existsSync(path)) mkdirSync(path);
     }
 }
+
+beforeEach(() => mkdirSync(ROOT));
+afterEach( () => rmdirSync(ROOT, { recursive: true}));
+
+test('should throw an error if the content is empty', () => {
+    Root.addFolder(" ");
+    expect(() => new Path(ROOT)).toThrow(EvalError);
+});
+
+test('should consider if the content contains only one character', () => {
+    Root.addFolder("F");
+    const path = new Path(ROOT);
+    path.renameContents();
+    expect(readdirSync(ROOT).includes("f")).toBeTruthy();
+});
 
 test('should remove spaces, transform to lowercase letter and exchange space for a hyphen: Test 1 -> test-1', () => {
     Root.addFolder("Folder Test 1");
@@ -32,10 +47,10 @@ test('should rename considering destination directory', () => {
     expect(readdirSync(`./test/${destiny}`).includes("folder-test-3")).toBeTruthy();
 });
 
-// test('should remove multiple hyphens and also at the beginning and end of the content', () => {
-//     Root.addFolder("-FolderTest-- 4-");
-//     const destiny = 'other-destiny';
-//     const path = new Path(ROOT, `./test/${destiny}`);
-//     path.renameContents();
-//     expect(readdirSync(`./test/${destiny}`).includes("folder-test-4")).toBeTruthy();
-// });
+// // test('should remove multiple hyphens and also at the beginning and end of the content', () => {
+// //     Root.addFolder("-FolderTest-- 4-");
+// //     const destiny = 'other-destiny';
+// //     const path = new Path(ROOT, `./test/${destiny}`);
+// //     path.renameContents();
+// //     expect(readdirSync(`./test/${destiny}`).includes("folder-test-4")).toBeTruthy();
+// // });
